5997888afa657b1a3e15849e1319d5aa191624b4
Aneet D Narendranath, PhD <dnaneet@mtu.edu>
2020-04-13 14:27:49 -0400

    Add files via upload

---
+++ footy/cup4.py
@@ -0,0 +1,213 @@
+'''
+Single-leg knockout competition for 8 teams
+'''
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+import time
+import pdb
+
+
+
+'''
+Function definitions
+'''
+nSamples = 4000; #This should be defined as a global variable
+
+'''
+Calculation of relative rank:
+Strength of team 1 wrt team 2
+If team-1 has a greater rank than team-2,
+team-1 has a greater probability of winning
+against team-2
+'''
+def relative_rank(t1r, t2r):
+    if(t1r < t2r):
+        return np.round(t2r/t1r)
+    else:
+        return np.round(t1r/t2r) 
+
+'''
+The delta value decides the strength of one 
+team against the other.  
+'''
+
+def delta(t1r, t2r):
+    if(t1r < t2r):
+        return -np.round(t2r/t1r) #team-1 is stronger than team-2
+    else:
+        return np.round(t1r/t2r) #team-2 is stronger than team-1
+        
+def strength_factor_evaluate(t1r, t2r, n):
+    if(np.max(t1r, t2r)/np.min(t1r, t2r)>=6): 
+        return 6
+    else:
+        return nSamples/2000        
+
+'''
+football match simulator
+'''
+def football_match(ts, g, team_1_advance, team_2_advance, team_1_name, team_2_name):
+  k=1;  
+  team_1_score=np.array([0])
+  team_2_score=np.array([0])
+  added_time = np.random.randint(7)
+  #pdb.set_trace() 
+  for i in range(89 + added_time): #begin loop
+    p = np.random.randint(nSamples)
+    if(p>=ts):
+        team_1_advance = np.append(team_1_advance,[1], axis=0)
+        team_2_advance= np.append(team_2_advance,[0], axis=0)
+    else:
+        team_1_advance= np.append(team_1_advance,[0], axis=0)
+        team_2_advance= np.append(team_2_advance,[1], axis=0)  
+    #n=np.int(0.5*g + np.random.randint(5))
+    if(k==1):
+        g1 = g
+        g2 = g
+        k=k+1
+    n1 = g1
+    n2 = g2
+    if(np.cumsum(team_1_advance[-n1:])[-1]>=n1):
+        team_1_score = team_1_score + 1
+        g1 = g1 + 1
+    if(np.cumsum(team_2_advance[-n2:])[-1]>=n2):        
+        team_2_score = team_2_score + 1 #end loop
+        g2 = g2 + 1     
+    
+    if(i%15 == 0): print('Minute #:',i , team_1_name, ':', team_1_score, team_2_name, ':', team_2_score)    
+
+  #score tallying  
+  if((team_1_score >=7 or team_2_score >=7) and (team_1_score == team_2_score)): 
+    team_1_score = np.random.randint([3])
+    team_2_score = np.random.randint([3])
+    
+  if((team_1_score >=7 or team_2_score >=7) and (team_1_score > team_2_score)): 
+    team_1_score = 4
+    team_2_score = 2  
+    
+  if((team_1_score >=7 or team_2_score >=7) and (team_1_score < team_2_score)): 
+    team_1_score = 2
+    team_2_score = 4       
+    
+  if(team_1_score != team_2_score):
+    print(team_1_name, ':', team_1_score, team_2_name, ':', team_2_score, file=f1)
+    print(team_1_name, ':', team_1_score, team_2_name, ':', team_2_score) #final scoreline is presented
+    return team_1_score, team_2_score, team_1_advance, team_2_advance #final scores and "possession" are returned 
+  elif(team_1_score == team_2_score):
+    while(team_1_score == team_2_score):
+        team_1_score = np.random.randint([3])
+        team_2_score = np.random.randint([3])
+    print(team_1_name, ':', team_1_score, team_2_name, ':', team_2_score, '(p)', file=f1)
+    print(team_1_name, ':', team_1_score, team_2_name, ':', team_2_score, '(p)') #final scoreline is presented    
+    return team_1_score, team_2_score, team_1_advance, team_2_advance #final scores and "possession" are returned
+
+'''
+Import full SPI data
+'''
+
+os.system('clear')
+team_list = pd.read_csv('./spi_global_rankings.csv')
+team_list['rank'] = team_list.index
+#print(team_list.head(5))
+print('SPI data imported')
+time.sleep(1)
+
+
+eps=0.001;
+
+worst_defense_val = np.max(team_list['def'])
+best_defense_val = np.min(team_list['def'])
+
+worst_attack_val = np.min(team_list['off'])
+best_attack_val = np.max(team_list['off'])
+
+rank_best = np.min(team_list['spi'])
+rank_worst = np.max(team_list['spi'])
+
+
+'''
+Knock-out cup simulation
+'''
+
+#df = pd.read_csv('match_listing_4.csv')
+df = pd.read_csv('league_trophy_8.csv')
+print('\n\nList of matches imported')
+time.sleep(1)
+print('\nList of games for the league trophy:\n\n', df)
+time.sleep(1)
+
+nGames = 1
+
+f1=open('./scorecard.txt', 'w+')
+team_1_score, team_2_score = 0,0
+next_round = np.array('') #Empty array has been created.  this will store 
+                          #names of teams that make it to the next round
+                          #This will be updated in the code below.
+
+#pdb.set_trace()
+
+nTotalRounds = int(np.log(len(df)*2)/np.log(2))
+matches_in_this_round = len(df)
+
+round = 1;
+j=0; #team number for subsequent rounds
+#while(round <= len(df) - 1): #nTotalRounds
+while(round <= nTotalRounds):
+    for match in range(matches_in_this_round):    
+        if(round <= len(df)-1):                
+            print('\n\nThis is match day:',match+1, 'of round ', round)
+            if(round==1):            
+                team_1_name = df['team 1'].iloc[match]
+                team_2_name = df['team 2'].iloc[match]
+            else:
+                team_1_name = next_round[j]             
+                team_2_name = next_round[j+1]
+                j = j+2 #next pair for next round
+            
+            team_1_dfx = float(np.abs(worst_defense_val - team_list['def'][team_list['name']== team_1_name])/worst_defense_val)
+            team_1_atx = float(team_list['off'][team_list['name']== team_1_name]/best_attack_val)
+            team_1_rank = int(team_list['rank'][team_list['name'] == team_1_name])
+            team_1_advance = np.array([0])
+            
+            team_2_dfx = float(np.abs(worst_defense_val - team_list['def'][team_list['name']== team_2_name])/worst_defense_val)
+            team_2_atx = float(team_list['off'][team_list['name']== team_2_name]/best_attack_val)
+            team_2_rank = int(team_list['rank'][team_list['name'] == team_2_name])
+            team_2_advance = np.array([0])
+            print(team_1_name, 'vs',  team_2_name, '\n\n')
+            print('\nThe whistle blows and the game starts!')
+            time.sleep(.1)
+            #strength_factor = strength_factor_evaluate(float(team_1_rank), float(team_2_rank), nSamples)
+            strength_factor = nSamples/2000
+            team_strength = 0.5*nSamples + np.abs(team_1_rank - team_2_rank)*strength_factor*delta(team_1_rank, team_2_rank)
+            gp = np.int(7 - team_1_dfx - team_2_dfx + team_1_atx + team_2_atx);
+            t1_score, t2_score, t1_possession, t2_possession = football_match(team_strength, gp, team_1_advance, team_2_advance, team_1_name, team_2_name)
+            if(t1_score > t2_score): 
+                next_round = np.append(next_round, team_1_name)
+                champs = team_1_name
+            else: 
+                next_round = np.append(next_round, team_2_name)        
+                champs = team_2_name
+            #print("this is t1 score: ", t1_score)
+            #print("this is t2 score: ", t2_score)
+            
+            possession = np.vstack((t1_possession, t2_possession))
+            plt.imshow(possession, cmap='jet')
+            fileName = team_1_name +'_v_' + team_2_name + '.pdf'
+            plt.savefig(fileName, bbox_inches='tight', dpi=400)
+                
+            input("Press Enter to continue...\n\n")
+    round = round + 1 #increment round #
+    next_round = next_round[-matches_in_this_round:]
+    matches_in_this_round = int(matches_in_this_round/2) #next round has half the matches of previous round
+    j=0;        
+
+print('\n')
+print('****************')
+print('This is it!!!')
+time.sleep(.6)
+print(champs, ' are the Champions!')
+print('****************')
+f1.close() #close file
+#eof
